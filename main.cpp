ec9b9952d08d01ce61238f5d72893ce32f0e1a71
Konstantin Kolodiy <bone@land.ru>
2012-06-23 21:45:22 +0900

    Only o rientation portrait

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
     QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockPortrait);
     viewer.setMainQmlFile(QLatin1String("qml/luxmeter/main.qml"));
     viewer.showExpanded();
 
